0bc0d78882244cea00968d92296d1685218e33df
anberns <anberns@gmail.com>
2018-04-23 15:13:14 -0400

    multi-threading for server

--- pi_server.py
+++ pi_server.py
@@ -1,5 +1,7 @@
 # Aaron Berns
 
+import thread
+import threading
 from socket import *
 import sys
 import time
@@ -7,6 +9,9 @@ import Adafruit_SSD1306
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+
+# thread lock
+lock = threading.Lock()
 
 # setup lcd, from Adafruit_Python_SSD1306
 RST = None
@@ -22,24 +27,61 @@ height = disp.height
 image = Image.new('1', (width, height))
 draw = ImageDraw.Draw(image)
 draw.rectangle((0,0,width,height), outline=0, fill=0)
-#padding = -2
 
 # display zones
-top = 0 #padding
+top = 0 
 ztop0 = top
 zbottom0 = ztop0+8
 ztop1 = zbottom0+8 
 zbottom1 = ztop1+8
 ztop2 = zbottom1+2 
 zbottom2 = ztop2+8
-
-botton = height #-padding
+bottom = height 
 x=0
+
+# display options
 font = ImageFont.load_default()
 
+# sockets
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', int(sys.argv[1]))) 
 serverSocket.listen(1)
+
+def soundEvent():
+	print ("sound")
+
+	lock.acquire()
+	draw.rectangle((0,ztop1,width,zbottom1), outline=0, fill=0)
+	draw.text((x, ztop1), "sound", font=font, fill=255)
+	disp.image(image)
+	disp.display()
+	lock.release()
+	time.sleep(2)
+	lock.acquire()
+	draw.rectangle((0,ztop1,width,zbottom1), outline=0, fill=0)
+	disp.image(image)
+	disp.display()
+	lock.release()
+
+	connectionSocket.send(("rec").encode())
+
+def motionEvent():
+	print ("motion")
+
+	lock.acquire()
+	draw.rectangle((0,ztop2,width,zbottom2), outline=0, fill=0)
+	draw.text((x, ztop2), "motion", font=font, fill=255)
+	disp.image(image)
+	disp.display()
+	lock.release()
+	time.sleep(2)
+	lock.acquire()
+	draw.rectangle((0,ztop2,width,zbottom2), outline=0, fill=0)		
+	disp.image(image)
+	disp.display()
+	lock.release()
+
+	connectionSocket.send(("rec").encode())
 
 # loop through connections from listening port until CTRL-C
 while 1:
@@ -55,39 +97,15 @@ while 1:
 	disp.image(image)
 	disp.display()
 
-	# need to multithread
 	while(1):
 		message = connectionSocket.recv(1024).decode()
 	
+		# sensor concurrency
 		if message == "sound":
-			print ("sound")
-
-			# write to lcd goes here
-			draw.rectangle((0,ztop1,width,zbottom1), outline=0, fill=0)
-			draw.text((x, ztop1), "sound", font=font, fill=255)
-			disp.image(image)
-			disp.display()
-			time.sleep(2)
-			draw.rectangle((0,ztop1,width,zbottom1), outline=0, fill=0)
-			disp.image(image)
-			disp.display()
-
-			connectionSocket.send(("rec").encode())
+			thread.start_new_thread(soundEvent, ())
 
 		elif message == "motion":
-			print ("motion")
-		
-			# write to lcd goes here
-			draw.rectangle((0,ztop2,width,zbottom2), outline=0, fill=0)
-			draw.text((x, ztop2), "motion", font=font, fill=255)
-			disp.image(image)
-			disp.display()
-			time.sleep(2)
-			draw.rectangle((0,ztop2,width,zbottom2), outline=0, fill=0)		
-			disp.image(image)
-			disp.display()
-
-			connectionSocket.send(("rec").encode())
+			thread.start_new_thread(motionEvent, ())
 
 		elif message == "disconnect":
 			break
@@ -108,3 +126,5 @@ while 1:
 	disp.clear()
 	disp.display()
 	connectionSocket.close()
+
+
